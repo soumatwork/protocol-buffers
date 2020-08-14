71ba17999950e090b97e6da0adacf8d946fc86b0
soumatwork <soum.at.work@gmail.com>
2020-08-15 00:31:06 +1000

    Refactor Main

--- java/src/main/java/com/soumc/Main.java
+++ java/src/main/java/com/soumc/Main.java
@@ -1,31 +1,32 @@
 package com.soumc;
 
-import com.soumc.io.ProtoBufReader;
-import com.soumc.io.ProtoBufWriter;
-import com.soumc.io.Reader;
-import com.soumc.io.Writer;
+import com.soumc.io.*;
 import com.soumc.models.AddressBookProtos;
 
 import java.io.IOException;
 
 public class Main {
+  static private final int PERSONS = 1000000;
+
   public static void main(String[] args) throws Exception {
-    write(args[0]);
-    read(args[0]);
+    System.out.println("protobuf");
+    write(new ProtoBufWriter<>(args[0]));
+    read(new ProtoBufReader<>(args[0], AddressBookProtos.AddressBook.parser()));
+
+    System.out.println("json");
+    write(new JsonWriter<>(args[1]));
+    read(new JsonReader<>(args[1]));
   }
 
-  private static void write(String path) throws IOException {
+  private static void write(Writer<AddressBookProtos.AddressBook> writer) throws IOException {
     long start = System.currentTimeMillis();
-    Writer<AddressBookProtos.AddressBook> writer = new ProtoBufWriter<>(path);
-    new AddressBookWriter(writer).write(10);
+    new AddressBookWriter(writer).write(PERSONS);
     System.out.println("Time taken to write: " + (System.currentTimeMillis() - start));
   }
 
-  private static void read(String path) throws IOException {
+  private static void read(Reader<AddressBookProtos.AddressBook> reader) throws IOException {
     long start = System.currentTimeMillis();
-    Reader<AddressBookProtos.AddressBook> reader
-        = new ProtoBufReader<>(path, AddressBookProtos.AddressBook.parser());
-    new AddressBookReader(reader).read();
+    System.out.println(new AddressBookReader(reader).read() == PERSONS);
     System.out.println("Time taken to read: " + (System.currentTimeMillis() - start));
   }
 }
