820a63de6918986e81464285bb5c2652a584465d
soumatwork <soum.at.work@gmail.com>
2020-08-27 01:08:18 +1000

    Add logs

--- protobuf-ruby/lib/main.rb
+++ protobuf-ruby/lib/main.rb
@@ -11,9 +11,11 @@ class Main
     NUMBER_OF_PERSON = 50_000
     def process
       address_book = AddressBookCreator.address_book(NUMBER_OF_PERSON)
+      p 'protobuf'
       write(address_book)
       read
 
+      p 'json'
       write_json(address_book)
       read_json
     end
@@ -21,27 +23,35 @@ class Main
     private
 
     def write(address_book)
+      starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
       File.open('../addressbook.data', 'wb') do |f|
-        f.write(address_book.to_proto)
+        f.write(Proto::AddressBook.encode(address_book))
       end
+      p "Time taken to write: #{Process.clock_gettime(Process::CLOCK_MONOTONIC) - starting} seconds"
     end
 
     def read
+      starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
       File.open('../addressbook.data', 'rb') do |f|
         p Proto::AddressBook.decode(f.read).people.count == NUMBER_OF_PERSON
       end
+      p "Time taken to read: #{Process.clock_gettime(Process::CLOCK_MONOTONIC) - starting} seconds"
     end
 
     def write_json(address_book)
+      starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
       File.open('../addressbook.json', 'wb') do |f|
         f.write(Proto::AddressBook.encode_json(address_book))
       end
+      p "Time taken to write: #{Process.clock_gettime(Process::CLOCK_MONOTONIC) - starting} seconds"
     end
 
     def read_json
+      starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
       File.open('../addressbook.json', 'rb') do |f|
         p Proto::AddressBook.decode_json(f.read).people.count == NUMBER_OF_PERSON
       end
+      p "Time taken to read: #{Process.clock_gettime(Process::CLOCK_MONOTONIC) - starting} seconds"
     end
   end
 end
--- protobuf-scala/README.md
+++ protobuf-scala/README.md
@@ -1,1 +1,2 @@
 Scala
+`protoc --java_out=src/main/java/ --proto_path=src/main/java/com/soumc/models addressbook.proto`
